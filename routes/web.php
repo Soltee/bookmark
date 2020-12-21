d4147395d763789031a5376b64b176087f89a7d6
Soltee <soltee.13@gmail.com>
2020-12-21 19:56:13 +0545

    setup laravel-inertia , react & typescript

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    // return view('dashboard');
+
+    return Inertia::render('Home/Index', [
+        ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
